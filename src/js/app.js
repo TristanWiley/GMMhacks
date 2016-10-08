6902c16e485c51532ba25eaee4ab4938ca0f5a75
System Administrator <root@Ians-MacBook-Pro.local>
2016-10-08 11:21:05 -0400

    iApps

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,53 @@
 var vinElem = document.getElementById('vin');
 
+function startSession() {
+  var retObj = gmint.speech.createStartSpeechRecSession();
+  var session = gmint[retObj.objName].StartSpeechRecSessionInternal();
+
+  return gmint[retObj.objName].getId();
+}
+
+function startRecording() {
+  var retObj = gmint.speech.createStartRecording();
+  gmint[retObj.objName].StartRecordingInternal({
+    "intro": "Hello from the world!",
+    "maxRecordingWindow": 10000,
+  });
+
+  return gmint[retObj.objName].getId();
+}
+
+var session = startSession();
+
+
+/*
+gm.speech.startSpeechRecSession(function () {
+  vinElem.innerHTML = gmint.appmanager.getAppsList();
+
+  gm.speech.startRecording(function () {
+    vinElem.innerHTML = "9";
+  }, function (e) {
+    vinElem.innerHTML = e + " potato";
+  }, {
+
+  });
+});
+
 gm.info.getCurrentPosition(function (position) {
+  console.log("Current Position " + position);
   vinElem.innerHTML = 'Lat: ' + position.coords.latitude + ' Long: ' + position.coords.longitude;
 });
 
-gm.info.watchPosition(function (position) {
-  vinElem.innerHTML = 'Lat: ' + position.coords.latitude + ' Long: ' + position.coords.longitude;
-}, function () {
-  console.log("GPS failed!");
-}, null, true);
+var id = gm.info.watchPosition(processPosition, processFailure, true);
+
+function processPosition(position){
+  var lat = position.coords.latitude;
+  var lng = position.coords.longitude;
+  console.log(lat + " " + lng);
+  vinElem.innerHTML = 'Lat: ' + lat + ' Long: ' + lng;
+}
+
+function processFailure(){
+  console.log("GPS FAILURE");
+}
+*/
---
+++ sync.sh
@@ -0,0 +1,4 @@
+cd src
+rsync -a -I --progress . root@172.17.0.1:/var/opt/bosch/dynamic/internet/apps/1251
+
+cd ..
